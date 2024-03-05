039e858d08463fb3303a840aad1a739eeffb4f7f
Enzo Renard <60904@etu.he2b.be>
2024-03-05 18:55:14 +0100

    Position : add ctor, op + implementation

--- include/Position.h
+++ include/Position.h
@@ -1,11 +1,20 @@
 #ifndef TETRIS_DEV4_PROJET_POSITION_H
 #define TETRIS_DEV4_PROJET_POSITION_H
+#include "Direction.hpp"
 
 class Position {
     int x;
     int y;
 public:
-    Position(int x, int y);
+    Position(int x = 0, int y = 0);
+    int get_x() const { return x; }
+    int get_y() const { return y; }
+    void operator+=(const Direction2D& direction);
+    void operator-=(const Direction2D& direction);
 };
+Position operator+(const Position &position, const Direction2D& direction);
+Position operator+(const Direction2D& direction, const Position &position);
+Position operator-(const Position &position, const Direction2D& direction);
+Position operator-(const Direction2D& direction, const Position &position);
 
 #endif //TETRIS_DEV4_PROJET_POSITION_H
---
+++ src/model/Position.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Enzo Renard on 05/03/2024.
+//
+#include "Position.h"
+#include "Direction.hpp"
+#include <utility>
+
+Position::Position(int x, int y) : x(x), y(y) {}
+Position operator+(const Position &position, const Direction2D& direction) {
+    return {position.get_x() + direction.first, position.get_y() + direction.second};
+}
+Position operator+(const Direction2D& direction, const Position &position) {
+    return position + direction;
+}
+Position operator-(const Position &position, const Direction2D& direction) {
+    return {position.get_x() - direction.first, position.get_y() - direction.second};
+}
+Position operator-(const Direction2D& direction, const Position &position) {
+    return position - direction;
+}
+void Position::operator+=(const Direction2D& direction) {
+    this->x += direction.first;
+    this->y += direction.second;
+}
+void Position::operator-=(const Direction2D& direction) {
+    this->x -= direction.first;
+    this->y -= direction.second;
+}
