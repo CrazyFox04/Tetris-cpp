7add02fb085eefc41bbbc8f29d6ddcd651a8f1f0
Enzo Renard <60904@etu.he2b.be>
2024-03-21 21:06:32 +0100

    Board : add tests

--- test/Board.cpp
+++ test/Board.cpp
@@ -32,32 +32,107 @@ TEST(Board, add_tetromino) {
 
 TEST(Board, move_active_tetromino) {
     Board b(5, 10, 1);
-    Tetromino t {1, Position(0, 0), {Position(0, 0), Position(1, 0), Position(2, 0), Position(3, 0)}};
+    Tetromino t{1, Position(0, 0), {Position(0, 0), Position(1, 0), Position(2, 0), Position(3, 0)}};
     b.addTetromino(t);
-    b.moveActiveTetromino(Direction2D(0,1)); // Down
+    b.moveActiveTetromino(Direction2D(0, 1)); // Down
     ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_x(), 0);
     ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_y(), 1);
 }
 
-TEST(Board, move_active_tetromino_out_of_bounds) {
+TEST(Board, move_active_tetromino_4dirs_happy_path) {
     Board b(5, 10, 1);
-    Tetromino t {1, Position(0, 0), {Position(0, 0), Position(1, 0), Position(2, 0), Position(3, 0)}};
+    Tetromino t{1, Position(0, 0), {Position(0, 0), Position(1, 0), Position(2, 0), Position(3, 0)}};
     b.addTetromino(t);
-    b.moveActiveTetromino(Direction2D(0,-1)); // Up
+    b.moveActiveTetromino(Direction2D(0, 1)); // Down
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_y(), 1);
+    b.moveActiveTetromino(Direction2D(1, 0)); // Right
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_x(), 1);
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_y(), 1);
+    b.moveActiveTetromino(Direction2D(0, -1)); // Up
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_x(), 1);
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_y(), 0);
+    b.moveActiveTetromino(Direction2D(-1, 0)); // Left
     ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_x(), 0);
     ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_y(), 0);
 }
 
-TEST(Board, move_active_tetromino_collision) { // todo : need to fix this test or his code !
+TEST(Board, move_active_tetromino_out_of_bounds) {
     Board b(5, 10, 1);
-    Tetromino t {1, Position(0, 0), {Position(0, 0), Position(1, 0), Position(2, 0), Position(3, 0)}};
+    Tetromino t{1, Position(0, 0), {Position(0, 0), Position(1, 0), Position(2, 0), Position(3, 0)}};
     b.addTetromino(t);
-    b.moveActiveTetromino({0,3}); // Down
+    b.moveActiveTetromino(Direction2D(0, -1)); // Up
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_y(), 0);
+}
+
+TEST(Board, move_active_tetromino_collision) {
+    Board b(5, 10, 1);
+    Tetromino t{1, Position(0, 0), {Position(0, 0), Position(1, 0), Position(2, 0), Position(3, 0)}};
     b.addTetromino(t);
-    b.moveActiveTetromino({0,1}); // Down
+    b.moveActiveTetromino({0, 3});
+    b.addTetromino(t);
+    b.moveActiveTetromino({0, 1});
     ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_x(), 0);
     ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_y(), 1);
-    b.moveActiveTetromino({0,1});
+    b.moveActiveTetromino({0, 1});
     ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_x(), 0);
-    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_y(), 1);
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_y(), 2);
+    b.moveActiveTetromino({0, 1});
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_y(), 2);
+}
+
+TEST(Board, rotate_active_tetromino) {
+    Board b(10, 10, 1);
+    Tetromino t{1, Position(0, 0), {Position(0, 0), Position(1, 0), Position(2, 0), Position(3, 0)}};
+    b.addTetromino(t);
+    b.moveActiveTetromino({0,4});
+    b.rotateActiveTetromino(Rotation::COUNTERCLOCKWISE);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(0).get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(0).get_y(), 4);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(1).get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(1).get_y(), 3);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(2).get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(2).get_y(), 2);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(3).get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(3).get_y(), 1);
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_y(), 4);
+}
+
+TEST(Board, rotate_active_tetromino_out_of_bounds) {
+    Board b(5, 10, 1);
+    Tetromino t{1, Position(0, 0), {Position(0, 0), Position(1, 0), Position(2, 0), Position(3, 0)}};
+    b.addTetromino(t);
+    b.rotateActiveTetromino(Rotation::CLOCKWISE);
+    // No rotation should have been done
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(0).get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(0).get_y(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(1).get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(1).get_y(), 1);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(2).get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(2).get_y(), 2);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(3).get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(3).get_y(), 3);
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_ref_position().get_y(), 0);
+}
+
+TEST(Board, rotate_active_tetromino_collision) {
+    Board b(5, 10, 1);
+    Tetromino t{1, Position(0, 0), {Position(0, 0), Position(1, 0), Position(2, 0), Position(3, 0)}};
+    b.addTetromino(t);
+    b.rotateActiveTetromino(Rotation::CLOCKWISE);
+    b.moveActiveTetromino({0, 1});
+    b.addTetromino(t);
+    b.rotateActiveTetromino(Rotation::CLOCKWISE);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(0).get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(0).get_y(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(1).get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(1).get_y(), 1);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(2).get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(2).get_y(), 2);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(3).get_x(), 0);
+    ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(3).get_y(), 3);
 }
