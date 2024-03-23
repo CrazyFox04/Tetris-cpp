cce7fff77dd53d726e704bb4867f59ffe353486c
60189 <60189@etu.he2b.be>
2024-03-23 16:43:52 +0100

    test bagview

--- test/BagViewTest.cpp
+++ test/BagViewTest.cpp
@@ -8,8 +8,8 @@
 TEST(BagViewTest, drawNextTetromino) {
     BagView bv;
     Tetromino t1 { 1, Position(0, 1), {Position(-2, 0), Position(-1, 0), Position(0, 0), Position(1, 0)} }; // I
-    bv.drawNextTetromino(t1);
+    EXPECT_NO_THROW(bv.drawNextTetromino(t1));
     Tetromino t2 { 2, Position(0, 0), {Position(0, 0), Position(1, 0), Position(0, 1), Position(1, 1)} }; // O
-    bv.drawNextTetromino(t2);
+    EXPECT_NO_THROW(bv.drawNextTetromino(t2));
 }
 
