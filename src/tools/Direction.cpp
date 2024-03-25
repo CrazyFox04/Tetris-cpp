135261a149c57fb2a5a583daf7e4d3b687cc01a4
Enzo Renard <60904@etu.he2b.be>
2024-03-25 09:12:35 +0100

    Fix Direction::Down pair

--- src/tools/Direction.cpp
+++ src/tools/Direction.cpp
@@ -4,5 +4,5 @@
 #include "Direction.h"
 
 const Direction2D Direction::RIGHT = std::make_pair(1,0);
-const Direction2D Direction::DOWN = std::make_pair(0,-1);
+const Direction2D Direction::DOWN = std::make_pair(0,1);
 const Direction2D Direction::LEFT = std::make_pair(-1,0);
