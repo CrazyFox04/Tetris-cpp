04c0605477f0cc2e29ae8086e0c8f080263eaf24
Enzo Renard <60904@etu.he2b.be>
2024-03-25 23:39:41 +0100

    Board : add and adapt tests accordingly

--- test/Board.cpp
+++ test/Board.cpp
@@ -30,7 +30,7 @@ TEST(Board, constructor_with_parameters_too_small) {
 TEST(Board, constructor_with_parameters_edge) {
     int maxHeight = 0;
     int maxWidth = 0;
-    for (const auto & available_tetromino : Bag::getInstance().getAvailableTetrominos()) {
+    for (const auto&available_tetromino: Bag::getInstance().getAvailableTetrominos()) {
         if (available_tetromino.get_height() > maxHeight) {
             maxHeight = available_tetromino.get_height();
         }
@@ -39,7 +39,7 @@ TEST(Board, constructor_with_parameters_edge) {
         }
     }
     // Make the board 2times greater than max size of tetro
-    Board b(maxWidth*2+1, maxHeight*2+1, 1);
+    Board b(maxWidth * 2 + 1, maxHeight * 2 + 1, 1);
     ASSERT_EQ(b.getWidth(), maxWidth*2+1);
     ASSERT_EQ(b.getHeight(), maxHeight*2+1);
     ASSERT_THROW(Board b3(maxWidth - 1, maxHeight, 1), std::invalid_argument);
@@ -111,7 +111,7 @@ TEST(Board, rotate_active_tetromino) {
     Board b(10, 10, 1);
     Tetromino t{1, Position(0, 0), {Position(0, 0), Position(1, 0), Position(2, 0), Position(3, 0)}};
     b.addTetromino(t);
-    b.moveActiveTetromino({0,4});
+    b.moveActiveTetromino({0, 4});
     b.rotateActiveTetromino(Rotation::COUNTERCLOCKWISE);
     ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(0).get_x(), 0);
     ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(0).get_y(), 4);
@@ -159,4 +159,38 @@ TEST(Board, rotate_active_tetromino_collision) {
     ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(2).get_y(), 2);
     ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(3).get_x(), 0);
     ASSERT_EQ(b.getActiveTetromino().get_relative_cells().at(3).get_y(), 3);
+}
+
+TEST(Board, remove_complete_lines) {
+    Board b(10, 10, 1);
+    Tetromino t{2, Position(0, 0), {Position(0, 0), Position(1, 0), Position(0, 1), Position(1, 1)}};
+    b.addTetromino(t);
+    b.moveActiveTetromino({-4, 8});
+    b.addTetromino(t);
+    b.moveActiveTetromino({-2, 8});
+    b.addTetromino(t);
+    b.moveActiveTetromino({0, 8});
+    b.addTetromino(t);
+    b.moveActiveTetromino({2, 8});
+    b.addTetromino(t);
+    b.moveActiveTetromino({4, 8});
+    b.addTetromino(t);
+    b.moveActiveTetromino({6, 8});
+    b.addTetromino(t);
+    b.moveActiveTetromino({8, 8});
+    ASSERT_EQ(b.removeCompleteLines(), 2);
+    for (const auto&bit_references: b.getOccupied()) {
+        for (const auto&bit_reference: bit_references) {
+            ASSERT_EQ(bit_reference, false);
+        }
+    }
+}
+
+TEST(Board, game_over) {
+    Board b(10, 10, 1);
+    ASSERT_FALSE(b.isGameOver());
+    b.addTetromino(Bag::getInstance().getAvailableTetrominos().at(0));
+    ASSERT_FALSE(b.isGameOver());
+    b.addTetromino(Bag::getInstance().getAvailableTetrominos().at(0));
+    ASSERT_TRUE(b.isGameOver());
 }
