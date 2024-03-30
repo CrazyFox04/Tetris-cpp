15041d3ab5e1abe7f5694ad142f359bc78f70420
Enzo Renard <60904@etu.he2b.be>
2024-03-30 12:46:58 +0100

    Add Bag & Board documentation

--- include/Bag.h
+++ include/Bag.h
@@ -2,25 +2,85 @@
 #define TETRIS_DEV4_PROJET_BAG_H
 
 #include <vector>
-#include <forward_list>
 #include "Tetromino.h"
 
+/**
+ * Tetromino bag used in a Tetris game.
+ * Keep a single instance of the bag and provide the next Tetromino to be used.
+ * It cannot be empty, so it will be refilled when it is empty.
+ */
 class Bag {
-    static Bag* instance;
-    std::vector<Tetromino> possibleTetrominos;
-    std::vector<Tetromino> bag;
+    static Bag* instance; //!< Singleton instance of the bag
+    std::vector<Tetromino> possibleTetrominos; //!< All possible Tetrominos that can be in the bag
+    std::vector<Tetromino> bag; //!< Tetrominos that are in the bag
+
+    /**
+     * Add all possible tetrominos to the bag.
+     */
     void addTetrominosToBag();
+
+    /**
+     * Add legacies tetrominos to the bag and add it to the possible tetrominos.
+     */
     Bag();
+
+    /**
+     * Shuffle the bag.
+     */
     void shuffle();
 
 public:
-    Bag(const Bag &) = delete;
-    Bag &operator=(const Bag &) = delete;
+    /**
+     * \brief Remove copy constructor.
+     *
+     * The copy constructor is removed to ensure that only one instance of the bag exists.
+     */
+    Bag(const Bag&) = delete;
+
+    /**
+    * \briefRemove copy assignment operator.
+    *
+    * The copy assignment operator is removed to ensure that only one instance of the bag exists.
+    */
+    Bag& operator=(const Bag&) = delete;
+
+    /**
+     * Get the number of possible tetrominos.
+     * @return number of possible tetrominos
+     */
     int getNumberOfTetrominos() const;
-    static Bag & getInstance();
-    Tetromino &getNext();
+
+    /**
+     * Get the singleton instance of the bag.
+     * @return singleton instance of the bag
+     */
+    static Bag& getInstance();
+
+    /**
+     * \brief Get the next Tetromino in the bag and remove it.
+     *
+     * When the bag is empty, it will be refilled and shuffled.
+     * @return next Tetromino in the bag
+     */
+    Tetromino& getNext();
+
+    /**
+     * Get the next Tetromino in the bag without removing it.
+     * @throw std::runtime_error if bag is empty
+     * @return const reference to the next Tetromino in the bag
+     */
     const Tetromino& peekNext() const;
+
+    /**
+     * Get the number of Tetrominos in the bag.
+     * @return number of Tetrominos in the bag
+     */
     int size();
+
+    /**
+     * Get all possible Tetrominos that can be in the bag.
+     * @return copy of all possible Tetrominos that can be in the bag
+     */
     std::vector<Tetromino> getAvailableTetrominos() const;
 };
 
--- include/Board.h
+++ include/Board.h
@@ -5,37 +5,99 @@
 #include "Tetromino.h"
 #include "Direction.h"
 
+/**
+ * Tetromino Board used in a Tetris game.
+ */
 class Board {
-    int width;
-    int height;
-    bool gameOver;
-    std::vector<Tetromino> tetrominos;
-    std::vector<std::vector<bool>> occupied;
-    Position refPosition;
+    int width; //!< Width of the board
+    int height; //!< Height of the board
+    bool gameOver; //!< State of the board
+    std::vector<Tetromino> tetrominos; //!< Tetrominos that are in the board
+    std::vector<std::vector<bool>> occupied; //!< Summary of the occupied cells in the board
+    Position refPosition; //!< Reference position of the board
 
 private:
-    void initialize(int difficulty = 0);
+    /**
+     * \brief Initialize the board with a given difficulty.
+     *
+     * The board will be filled with occupied cells based on the difficulty.
+     * The number of lines filled will be the difficulty percentage of the height.
+     * @param difficulty [1;20] integer that will determine the fill percentage of the board
+     */
+    void initialize(int difficulty = 1);
+
+    /**
+     * \brief Clear a line of the board.
+     *
+     * Keep update the occupied and Tetrominos cells
+     * @param line line number (y-axis)
+     */
     void clearLine(int line);
+
+    /**
+     * Move Direction::DOWN every lines of the board
+     * @param line number of lines to push down
+     */
     void moveLinesDown(int line);
+
+    /**
+     * Clear the occupied vector of the active tetromino'cells positions
+     */
     void clearOccupiedForActiveTetromino();
-    bool isOutside(Tetromino& tetromino) const;
+
+    /**
+     * Check if tetromino's cells are in the board
+     * @param tetromino tetromino to check cells
+     * @return true if at least one cells is outside, false otherwise
+     */
+    bool isOutside(Tetromino&tetromino) const;
 
 public:
+    /**
+     * Cronstruct a 10 by 20 board with a difficulty of 20
+     * \see Board(int, int, int);
+     */
     Board();
+
+    /**
+     * Construct a Board with the given width, height and difficulty.
+     * The Board MUST be 2 times larger and 2 times higher than the maximum of possible tetrominos
+     * @param width width of the Board
+     * @param height height of the Board
+     * @param difficulty [1;20] integer that will determine the fill percentage of the board
+     * @throw std::invalid_argument if the board is not large or higher enough
+     */
     Board(int width, int height, int difficulty); // difficulty will determinate the percentage of occupied cells
-    void addTetromino(Tetromino& tetromino);
+    /**
+     * Add a tetromino to the Board
+     * @param tetromino
+     */
+    void addTetromino(Tetromino&tetromino);
+
     void moveActiveTetromino(Direction2D direction);
+
     void rotateActiveTetromino(Rotation rotation);
+
     bool isOutside(int row, int column) const;
+
     bool isOccupied(int row, int column) const;
+
     bool isLineComplete(int line) const;
+
     int removeCompleteLines();
+
     bool isGameOver() const;
+
     Tetromino& getActiveTetromino();
+
     const std::vector<Tetromino> getTetrominos() const;
+
     int getWidth() const;
+
     int getHeight() const;
+
     const std::vector<std::vector<bool>> getOccupied() const;
+
     Position getRefPosition() const;
 };
 
