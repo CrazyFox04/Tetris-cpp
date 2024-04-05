77e26b32daf2ccbe0cf05e1dcd9de76b01d0a966
Enzo Renard <60904@etu.he2b.be>
2024-04-05 15:12:59 +0200

    Fix memory leak

--- src/controller/QuitGameCommand.cpp
+++ src/controller/QuitGameCommand.cpp
@@ -1,10 +1,7 @@
 #include "QuitGameCommand.h"
 #include <cstdlib>
 
-#include "Bag.h"
-
 QuitGameCommand::QuitGameCommand() {}
 void QuitGameCommand::execute() {
-    free(&Bag::getInstance());
     exit(0);
 }
