6207d127eb70a8a0e9142236e96ebd02534f2ed1
Enzo Renard <60904@etu.he2b.be>
2024-04-26 14:40:53 +0200

    Create configuration window

--- include/TetrisConfiguration.h
+++ include/TetrisConfiguration.h
@@ -5,11 +5,47 @@
 #ifndef TETRISCONFIGURATION_H
 #define TETRISCONFIGURATION_H
 #include <QWidget>
-class TetrisConfiguration : public QWidget {
-    QWidget window;
+#include <QFormLayout>
+#include <QLabel>
+#include <QMainWindow>
+#include <QCloseEvent>
+#include <QLineEdit>
 
+#include "GameSettings.hpp"
+
+class TetrisConfiguration : public QFormLayout {
+    QWidget window;
+    QLabel* boardWidthLabel;
+    QLabel* boardHeightLabel;
+    QLabel* startLevelLabel;
+    QLabel* targetLineLabel;
+    QLabel* targetTimeLabel;
+    QLabel* targetScoreLabel;
+    QLabel* difficultyLabel;
+    QLineEdit* boardWidthLineEdit;
+    QLineEdit* boardHeightLineEdit;
+    QLineEdit* startLevelLineEdit;
+    QLineEdit* targetLineLineEdit;
+    QLineEdit* targetTimeLineEdit;
+    QLineEdit* targetScoreLineEdit;
+    QLineEdit* difficultyLineEdit;
+
+    GameSettings* settings;
+
+    void configureWindow();
+
+    void createLabels();
+
+    void createLineEdits();
+
+    void addFields();
+
+    void createButtons();
+
+    void close();
 public:
     TetrisConfiguration();
-    int start(QApplication *myQtApp);
+
+    int start(QApplication* myQtApp);
 };
 #endif //TETRISCONFIGURATION_H
--- src/view/qtApp/tetrisConfiguration.cpp
+++ src/view/qtApp/tetrisConfiguration.cpp
@@ -4,13 +4,73 @@
 #include "TetrisConfiguration.h"
 #include <QWidget>
 #include <QApplication>
+#include <QLabel>
+#include <QComboBox>
+#include <QLineEdit>
+#include <QPushButton>
+#include "Game.h"
+#include "TetrisView.h"
+#include <QCloseEvent>
+
 
 TetrisConfiguration::TetrisConfiguration() {
     window.setWindowTitle("Tetris Configuration");
-    window.resize(300, 200);
+    configureWindow();
+    window.setLayout(this);
     window.show();
 }
 
-int TetrisConfiguration::start(QApplication *myQtApp) {
+
+void TetrisConfiguration::configureWindow() {
+    window.resize(300, 200);
+    createLabels();
+    createLineEdits();
+    addFields();
+    createButtons();
+    window.show();
+}
+
+void TetrisConfiguration::close() {
+    window.close();
+}
+
+void TetrisConfiguration::createButtons() {
+    QPushButton* startButton = new QPushButton("Start");
+    addRow(startButton);
+    connect(startButton, &QPushButton::clicked, this, &TetrisConfiguration::close);
+}
+
+void TetrisConfiguration::addFields() {
+    addRow(boardWidthLabel, boardWidthLineEdit);
+    addRow(boardHeightLabel, boardHeightLineEdit);
+    addRow(startLevelLabel, startLevelLineEdit);
+    addRow(targetLineLabel, targetLineLineEdit);
+    addRow(targetTimeLabel, targetTimeLineEdit);
+    addRow(targetScoreLabel, targetScoreLineEdit);
+    addRow(difficultyLabel, difficultyLineEdit);
+}
+
+void TetrisConfiguration::createLabels() {
+    boardWidthLabel = new QLabel("Board Width");
+    boardHeightLabel = new QLabel("Board Height");
+    startLevelLabel = new QLabel("Start Level");
+    targetLineLabel = new QLabel("Target Line");
+    targetTimeLabel = new QLabel("Target Time");
+    targetScoreLabel = new QLabel("Target Score");
+    difficultyLabel = new QLabel("Difficulty");
+}
+
+void TetrisConfiguration::createLineEdits() {
+    boardWidthLineEdit = new QLineEdit(QString::number(Board::DEFAULT_WIDTH));
+    boardHeightLineEdit = new QLineEdit(QString::number(Board::DEFAULT_HEIGHT));
+    startLevelLineEdit = new QLineEdit(QString::number(1));
+    targetLineLineEdit = new QLineEdit(QString::number(0));
+    targetTimeLineEdit = new QLineEdit(QString::number(0));
+    targetScoreLineEdit = new QLineEdit(QString::number(0));
+    difficultyLineEdit = new QLineEdit(QString::number(0));
+}
+
+
+int TetrisConfiguration::start(QApplication* myQtApp) {
     return myQtApp->exec();
 }
