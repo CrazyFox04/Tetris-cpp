79d3cdf0fdfc8ff88e4d8a6353312c9040a608e5
Enzo Renard <60904@etu.he2b.be>
2024-05-05 16:30:04 +0200

    Process arguments used

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+#include <iostream>
+
+#include "ApplicationTetris.h"
 #include "TetrisQtGUI.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
+    if (argc > 2) {
+        std::cerr << "Too many arguments !" << std::endl;
+        return 1;
+    }
+    if (argc == 2 && std::string(argv[1]) == "-c") {
+        ApplicationTetris app = ApplicationTetris();
+        app.run();
+    }
     TetrisQtGUI app = TetrisQtGUI();
     app.run(argc, argv);
+    return 0;
 }
