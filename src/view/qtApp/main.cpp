d3290957ee9eb6bd2a52c57265b8a2f3fc079c4f
Enzo Renard <60904@etu.he2b.be>
2024-05-05 16:23:52 +0200

    Clean headers

--- src/view/qtApp/main.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Julien Delcombel on 23/04/2024.
-//
-#include "TetrisView.h"
-
-int main(int argc, char **argv) {
-    TetrisView app = TetrisView(argc, argv);
-    return app.start();
-}
